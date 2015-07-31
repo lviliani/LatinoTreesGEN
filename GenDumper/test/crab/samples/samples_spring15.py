ce4deef4fba468f6a088fd2c5c373c13f69b998c
Andrea <massironi.andrea@gmail.com>
2015-07-31 18:49:22 +0200

    update for multicrab

---
+++ GenDumper/test/crab/samples/samples_spring15.py
@@ -0,0 +1,35 @@
+########
+# 25 ns
+########
+#        CRAB task name          DAS name                                                                                                              Latino name
+samples['25ns_WWTo2L2Nu']    = ['/WWTo2L2Nu_13TeV-powheg/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM',                                   ['label=WWTo2L2Nu',    'id=00001']
+samples['25ns_WZ']           = ['/WZ_TuneCUETP8M1_13TeV-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM',                            ['label=WZ',           'id=00002']]
+samples['25ns_WZTo3LNu']     = ['/WZTo3LNu_TuneCUETP8M1_13TeV-powheg-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM',               ['label=WZTo3LNu',     'id=00003']
+samples['25ns_ZZ']           = ['/ZZ_TuneCUETP8M1_13TeV-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v3/MINIAODSIM',                            ['label=ZZ',           'id=00004']]
+samples['25ns_TTTo2L2Nu']    = ['/TTTo2L2Nu_13TeV-powheg/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM',                                   ['label=TTTo2L2Nu',    'id=00005']
+samples['25ns_TTJets']       = ['/TTJets_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM',           ['label=TTJets',       'id=00006']
+samples['25ns_DYJetsToLL']   = ['/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v3/MINIAODSIM',  ['label=DYJetsToLL',   'id=00007']
+samples['25ns_WJetsToLNu']   = ['/WJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM',       ['label=WJetsToLNu',   'id=00008']
+samples['25ns_ZZ4l']         = ['/ZZTo4L_13TeV_powheg_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM',                              ['label=ZZ4l',         'id=00009']
+samples['25ns_ZZ2l2q']       = ['/ZZTo2L2Q_13TeV_amcatnloFXFX_madspin_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM',              ['label=ZZ2l2q',       'id=00010']
+samples['25ns_topTchannelAntitop'] = ['/ST_t-channel_antitop_4f_leptonDecays_13TeV-powheg-pythia8_TuneCUETP8M1/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM',  ['label=topTchannelAntitop', 'id=00011']
+samples['25ns_topTchannel']        = ['/ST_t-channel_top_4f_leptonDecays_13TeV-powheg-pythia8_TuneCUETP8M1/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM',      ['label=topTchannel',        'id=00012']
+samples['25ns_topTWantitop']       = ['/ST_tW_antitop_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M1/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM',      ['label=topTWantitop',       'id=00013']
+samples['25ns_topTWtop']           = ['/ST_tW_top_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M1/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM',          ['label=topTWtop',           'id=00014']
+samples['25ns_DYJetsToLL1050']     = ['/DYJetsToLL_M-10to50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM',             ['label=DYJetsToLL1050',     'id=00015']
+
+
+# Higgs
+samples['25ns_ggHWW120']     = ['/GluGluHToWWTo2L2Nu_M120_13TeV_powheg_JHUgen_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM',      ['label=ggHWW120',     'id=01120']
+samples['25ns_WpHWW130']     = ['/HWplusJ_HToWW_M130_13TeV_powheg_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM',      ['label=WpHWW130',     'id=03130']
+samples['25ns_ggHZZ4l125']     = ['/GluGluHToZZTo4L_M125_13TeV_powheg_JHUgen_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM',      ['label=ggHZZ125',     'id=11125']
+
+
+
+
+########
+# Alternative global configuration
+########
+# config.Data.outLFNDirBase = '/store/user/piedra/test'
+# config.Site.storageSite   = 'T2_ES_IFCA'
+# config.Data.outLFNDirBase = '/store/group/phys_higgs/cmshww/amassiro/RunII/test3Jul/'
