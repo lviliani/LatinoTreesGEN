08cda6af632c968e876c6250aacb107c3acdeaba
Andrea <massironi.andrea@gmail.com>
2015-08-11 17:59:26 +0200

    calculate weights

---
+++ GenDumper/test/CutPlotCount/README.md
@@ -0,0 +1,22 @@
+Plot tools
+====
+
+Calculate weights for aMC@NLO
+
+    cd /tmp/amassiro/
+    eosmount eos eos
+    cd -
+    
+    ls /tmp/amassiro/eos/cms/store/group/phys_higgs/cmshww/amassiro/RunII/GenDumper/
+    
+    # create list of files
+    
+    ls  --color=none  /tmp/amassiro/eos/cms/store/group/phys_higgs/cmshww/amassiro/RunII/GenDumper/ | \
+        awk '{print "ls --color=none /tmp/amassiro/eos/cms/store/group/phys_higgs/cmshww/amassiro/RunII/GenDumper/"$1"/*/*/*/GEN*.root &> "$1".txt"}' | /bin/sh
+        
+    # analyse list of files
+    ls /tmp/amassiro/eos/cms/store/group/phys_higgs/cmshww/amassiro/RunII/GenDumper/ | \
+        awk '{print "r99t count.cxx\\\(\\\""$1".txt\\\"\\\)"}' | /bin/sh
+    
+    
+        
---
+++ GenDumper/test/CutPlotCount/count.cxx
@@ -0,0 +1,41 @@
+
+//---- calculate effective weight for negative weight sample
+
+int count(std::string samples) {
+ 
+ std::cout << " ~~~~~ " << std::endl;
+ std::cout << " samples = " << samples << std::endl;
+ 
+ std::vector <std::string> v_nameFile;
+ 
+ std::ifstream indata;
+ std::string nameFile;
+ indata.open(samples.c_str()); // opens the file
+ if (!indata) { // file couldn't be opened
+  std::cerr << "Error: file " << argv[1] << " could not be opened" << std::endl;
+  return 1;
+ }
+ 
+ while ( !indata.eof() ) { // keep reading until end-of-file
+  indata >> nameFile;
+  v_nameFile.push_back(nameFile);
+ }
+ indata.close();
+ 
+ 
+ 
+ 
+
+ TChain* myTree = new TChain ("Analyzer/myTree", "myTree");
+ for (int iFile = 0; iFile < v_nameFile.size(); iFile++) {
+  myTree->Add(v_nameFile.at(iFile).c_str()); 
+ }
+ 
+ std::cout << " entries = " << myTree->GetEntries() << std::endl;
+ int Np = myTree->GetEntries("weightSM>0");
+ int Nm = myTree->GetEntries("weightSM<0");
+ std::cout << " weight = " << 1. * (Np - Nm) / (Np + Nm) << std::endl;
+ std::cout << " ~~~~~ " << std::endl;
+ 
+ return 0;
+}
